3915c884257b51f1a17bf5e9469c5fc9a76bfed1
Tim van Citters <timvancitters@Tims-MacBook-Pro.local>
2021-01-11 12:48:39 +0100

    going good good

---
+++ Module_01/ex01/ex01.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ex01.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: timvancitters <timvancitters@student.co      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/11 12:42:39 by timvancitte   #+#    #+#                 */
+/*   Updated: 2021/01/11 12:48:01 by timvancitte   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+void	memoryleak()
+{
+	std::string*	panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
