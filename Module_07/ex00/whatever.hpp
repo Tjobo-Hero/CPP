b02a1cc308c0e43d6fef0bc99e9f8db28f6e50f9
Tim van Citters <timvancitters@Tims-MacBook-Pro.local>
2021-02-25 16:24:24 +0100

    removed const reference for min and max

--- Module_07/ex00/whatever.hpp
+++ Module_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: timvancitters <timvancitters@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/25 14:04:51 by timvancitte   #+#    #+#                 */
-/*   Updated: 2021/02/25 16:16:34 by timvancitte   ########   odam.nl         */
+/*   Updated: 2021/02/25 16:23:53 by timvancitte   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 #include <string>
 
 template< typename T >								
-T const &		max(T const &first, T const &second)	
+T		max(T const &first, T const &second)	
 {
 	return ((first > second) ? first : second);
 }
 
 template< typename T >
-T const &		min(T  const &first, T const &second)
+T		min(T  const &first, T const &second)
 {
 	return ((first < second ) ? first : second);
 }
