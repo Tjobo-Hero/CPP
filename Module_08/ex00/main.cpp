83b8571b0ae1b5e2681602f49fd3279e3eaca4a2
Tim van Citters <timvancitters@Tims-MacBook-Pro.local>
2021-02-26 10:45:58 +0100

    finished ex00

--- Module_08/ex00/main.cpp
+++ Module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: timvancitters <timvancitters@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/25 17:09:30 by timvancitte   #+#    #+#                 */
-/*   Updated: 2021/02/25 17:32:59 by timvancitte   ########   odam.nl         */
+/*   Updated: 2021/02/26 10:45:17 by timvancitte   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,76 @@
 #include <list>
 #include <vector>
 
+void	setRand(void)
+{
+	static bool random = false;
+
+	if (!random)
+	{
+		srand(time(NULL));
+		random = true;
+	}
+	return;
+}
+
+int		random_int(int type)
+{
+	int 		randomInt = rand() % 100 + 1;
+	static int 	i;
+	static bool n = false;
+	
+	if (!n || i == 6)
+	{
+		i = 0;
+		n = true;
+	}
+	else
+		i += 1;
+	if (type == 1)
+		std::cout << "List: i: [" << i << "]" << " Random int: " << randomInt << std::endl;
+	else
+		std::cout << "Vector: i: [" << i << "]" << " Random int: " << randomInt << std::endl;
+	return randomInt;
+}
+
 int		main(void)
 {
-	std::list<int>			test1(5);
-	// std::list<std::string>		test2(5);
-	std::vector<std::string> test2(5);	
+	std::list<int>			lst1;
+	std::vector<int>		vec1;
 
-	std::vector<int>::const_iterator	it;
-	std::vector<int>::const_iterator	ite = test2.end();
+	// setRand();
+	for (int i = 0; i < 7; i++)
+	{
+		vec1.push_back(random_int(2));
+		if (i == 6)
+			for (int i = 0; i < 7; i++)
+				lst1.push_back(random_int(1));
+	}
 	
-	for(it = std::begin(test2))
+	// std::list<int>::const_iterator	it;
+	// std::list<int>::const_iterator	ite = lst1.end();
 
-	test1.push_back(1);
-	test1.push_back(2);
-	test1.push_back(3);
-	test1.push_back(4);
-	test1.push_back(5);
+	// for(it = lst1.begin(); it != ite; it++)
+	// {
+	// 	std::cout << *it << std::endl;
+	// }
 
-	easyfind(test1, 4);
+	try
+	{
+		easyfind(lst1, 79);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		easyfind(vec1, 50);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 	return (0);
 }
